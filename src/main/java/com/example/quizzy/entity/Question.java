84c47d130898fe67925e929eccc63108cad2ab44
Michal Kowal <michal.kowal.66@gmail.com>
2025-05-05 23:30:50 +0200

    Add initial implementation of question entity
    Set up ORM entity annotation, set id, map relationships, ensure table and column names

---
+++ src/main/java/com/example/quizzy/entity/Question.java
@@ -0,0 +1,41 @@
+package com.example.quizzy.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "questions")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "quiz_id")
+    private Quiz quiz;
+
+    @Enumerated(EnumType.STRING)
+    private QuestionType type;
+
+    private String text;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    private Integer position;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+    private List<Answer> answers;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+    private List<MatchPair> matchPairs;
+}
+
