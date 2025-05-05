08ea4898a529ec6bd512f3947e9ab0035c0f29d1
Michal Kowal <michal.kowal.66@gmail.com>
2025-05-05 23:40:47 +0200

    Add annotation pointing to a package containing db entities

--- src/main/java/com/example/quizzy/QuizzyApplication.java
+++ src/main/java/com/example/quizzy/QuizzyApplication.java
@@ -2,8 +2,10 @@ package com.example.quizzy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.example.quizzy.entity")
 public class QuizzyApplication {
 
 	public static void main(String[] args) {
