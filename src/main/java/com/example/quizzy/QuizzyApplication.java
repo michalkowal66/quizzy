b57dce94d89c316c8e363ed72360cbb74bedc407
Michal Kowal <michal.kowal.66@gmail.com>
2025-05-05 23:48:44 +0200

    Enable jpa repositories, choose package containing repositories

--- src/main/java/com/example/quizzy/QuizzyApplication.java
+++ src/main/java/com/example/quizzy/QuizzyApplication.java
@@ -3,9 +3,11 @@ package com.example.quizzy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan("com.example.quizzy.entity")
+@EnableJpaRepositories("com.example.quizzy.repository")
 public class QuizzyApplication {
 
 	public static void main(String[] args) {
