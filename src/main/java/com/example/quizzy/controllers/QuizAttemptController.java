8e0e8dcf8901b496d6b2a85a0c1f9a732fddb775
Michal Kowal <michal.kowal.66@gmail.com>
2025-06-18 20:08:53 +0200

    QuizAttemptController add submitAnswers endpoint, rename startQuizAttempt endpoint for better readability

--- src/main/java/com/example/quizzy/controllers/QuizAttemptController.java
+++ src/main/java/com/example/quizzy/controllers/QuizAttemptController.java
@@ -1,14 +1,13 @@
 package com.example.quizzy.controllers;
 
 import com.example.quizzy.dto.QuizAttemptStartResponseDto;
+import com.example.quizzy.dto.QuizSubmissionDto;
 import com.example.quizzy.services.QuizAttemptService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -22,10 +21,23 @@ public class QuizAttemptController {
      * @param quizId The ID of the quiz to start.
      * @return A DTO containing the new attempt ID and a list of playable questions.
      */
-    @PostMapping("/quizzes/{quizId}/attempts")
+    @PostMapping("/start/{quizId}")
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<QuizAttemptStartResponseDto> startQuizAttempt(@PathVariable Long quizId) {
         QuizAttemptStartResponseDto response = quizAttemptService.startQuiz(quizId);
         return ResponseEntity.ok(response);
+    }
+
+    /**
+     * Submits answers for a specific quiz attempt.
+     * @param attemptId The ID of the quiz attempt being submitted.
+     * @param submissionDto The DTO containing the list of answers.
+     * @return A no content response indicating success.
+     */
+    @PostMapping("/{attemptId}/submit")
+    @PreAuthorize("@quizAttemptSecurityService.isOwner(#attemptId, authentication)")
+    public ResponseEntity<Void> submitAnswers(@PathVariable Long attemptId, @Valid @RequestBody QuizSubmissionDto submissionDto) {
+        quizAttemptService.submitAnswers(attemptId, submissionDto);
+        return ResponseEntity.noContent().build();
     }
 }
